e0c8fe8ca170415a76425ac9d6ff50d35df50b58
Matthieu Barbe <brm3475b@etu.iut-tlse3.fr>
2022-04-11 12:44:28 +0200

    User,Teacher,Student

---
+++ src/main/java/com/example/myapplication/Student.kt
@@ -0,0 +1,5 @@
+package com.example.myapplication
+
+
+class Student(name:String) : UserBis(name) {
+}
---
+++ src/main/java/com/example/myapplication/UserBis.kt
@@ -0,0 +1,5 @@
+package com.example.myapplication
+
+open class UserBis(name:String) {
+    var name : String? = null
+}
